9fa62fb55c8c0b46620d4625db4d6c71741dc83a
jtreeves <jackson.t.reeves@gmail.com>
2023-12-02 18:01:47 -0500

    Add stricter typing

--- day_1/solution.py
+++ day_1/solution.py
@@ -1,7 +1,24 @@
+from typing import List
+
 digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 
 
-def solution():
+class NumberInLine:
+    def __init__(self, value: int | str, index: int) -> None:
+        self.value = value
+        self.index = index
+
+
+class SolutionResults:
+    def __init__(self, part_1: int, part_2: int) -> None:
+        self.part_1 = part_1
+        self.part_2 = part_2
+
+    def __repr__(self):
+        return f"SOLUTIONS\nPart 1: {self.part_1}\nPart 2: {self.part_2}"
+
+
+def solution() -> SolutionResults:
     chunk = extract_data_from_file()
     lines = break_chunk_into_lines(chunk)
     total = 0
@@ -10,10 +27,11 @@ def solution():
         final_digit = combine_first_and_last_numbers_from_line_into_new_number(
             numbers_in_line)
         total += final_digit
-    return total
+    results = SolutionResults(0, total)
+    return results
 
 
-def combine_first_and_last_numbers_from_line_into_new_number(numbers_array):
+def combine_first_and_last_numbers_from_line_into_new_number(numbers_array: List[NumberInLine]) -> int:
     first_and_last_numbers = determine_first_and_last_numbers_for_line(
         numbers_array)
     first_number = first_and_last_numbers[0]
@@ -23,18 +41,18 @@ def combine_first_and_last_numbers_from_line_into_new_number(numbers_array):
     return composite_number
 
 
-def determine_first_and_last_numbers_for_line(numbers_array):
-    sorted_substrings = sorted(numbers_array, key=lambda x: x['index'])
+def determine_first_and_last_numbers_for_line(numbers_array: List[NumberInLine]) -> List[int]:
+    sorted_substrings = sorted(numbers_array, key=lambda x: x.index)
     first_number_obj = sorted_substrings[0]
     last_number_obj = sorted_substrings[-1]
-    first_number = first_number_obj['value'] if str(first_number_obj['value']).isdigit(
-    ) else convert_string_to_number(first_number_obj['value'])
-    last_number = last_number_obj['value'] if str(last_number_obj['value']).isdigit(
-    ) else convert_string_to_number(last_number_obj['value'])
+    first_number = int(first_number_obj.value if str(first_number_obj.value).isdigit(
+    ) else convert_string_to_number(str(first_number_obj.value)))
+    last_number = int(last_number_obj.value if str(last_number_obj.value).isdigit(
+    ) else convert_string_to_number(str(last_number_obj.value)))
     return [first_number, last_number]
 
 
-def convert_string_to_number(digit: str):
+def convert_string_to_number(digit: str) -> int:
     if digit == 'one':
         return 1
     elif digit == 'two':
@@ -53,27 +71,27 @@ def convert_string_to_number(digit: str):
         return 8
     elif digit == 'nine':
         return 9
+    else:
+        return 0
 
 
-def find_all_numbers_in_line(line: str):
-    numbers_in_line = []
+def find_all_numbers_in_line(line: str) -> List[NumberInLine]:
+    numbers_in_line: List[NumberInLine] = []
     for digit in digits:
         index = line.find(str(digit))
         while index != -1:
-            numbers_in_line.append({
-                'value': digit,
-                'index': index
-            })
+            number_in_line = NumberInLine(digit, index)
+            numbers_in_line.append(number_in_line)
             index = line.find(str(digit), index + 1)
     return numbers_in_line
 
 
-def break_chunk_into_lines(chunk: str):
+def break_chunk_into_lines(chunk: str) -> List[str]:
     lines = chunk.split("\n")
     return lines
 
 
-def extract_data_from_file():
+def extract_data_from_file() -> str:
     file = open("data.txt", "r")
     data = file.read()
     file.close()
