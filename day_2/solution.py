3926c674c294ca263625b51f718f7d8b8c8292e4
jtreeves <jackson.t.reeves@gmail.com>
2024-12-02 01:16:54 -0500

    Attempt part 2 of day 2

--- day_2/solution.py
+++ day_2/solution.py
@@ -23,6 +23,15 @@ def determine_consistent_direction(vector: List[int]) -> bool:
     return consistent
 
 
+def determine_consistent_direction_with_dampener(vector: List[int], problems: int = 0) -> bool:
+    if problems > 1:
+        return False
+    for i in range(len(vector) - 2):
+        if (vector[i] < vector[i + 1] and vector[i + 1] > vector[i + 2]) or (vector[i] > vector[i + 1] and vector[i + 1] < vector[i + 2]):
+            return determine_consistent_direction_with_dampener(vector[:i] + vector[i:], problems + 1)
+    return True
+
+
 def determine_acceptable_variance(vector: List[int]) -> bool:
     for i in range(len(vector) - 1):
         difference = abs(vector[i] - vector[i + 1])
@@ -31,9 +40,29 @@ def determine_acceptable_variance(vector: List[int]) -> bool:
     return True
 
 
+def determine_acceptable_variance_with_dampener(vector: List[int], problems: int = 0) -> bool:
+    if problems > 1:
+        return False
+    for i in range(len(vector) - 1):
+        difference = abs(vector[i] - vector[i + 1])
+        if difference == 0 or difference > 3:
+            return determine_acceptable_variance_with_dampener(vector[:i] + vector[i:], problems + 1)
+    return True
+
+
 def determine_safety(vector: List[int]) -> bool:
     consistent = determine_consistent_direction(vector)
     acceptable = determine_acceptable_variance(vector)
+    safe = consistent and acceptable
+    return safe
+
+
+def determine_safety_with_dampener(vector: List[int]) -> bool:
+    print("VECTOR:", vector)
+    consistent = determine_consistent_direction_with_dampener(vector)
+    print("CONSISTENT:", consistent)
+    acceptable = determine_acceptable_variance_with_dampener(vector)
+    print("ACCEPTABLE:", acceptable)
     safe = consistent and acceptable
     return safe
 
@@ -48,12 +77,22 @@ def calculate_total_safe_reports(reports: List[str]) -> int:
     return count
 
 
+def calculate_total_safe_reports_with_dampener(reports: List[str]) -> int:
+    count = 0
+    for report in reports:
+        levels = extract_levels(report)
+        safe = determine_safety_with_dampener(levels)
+        if safe:
+            count += 1
+    return count
+
+
 def solve_problem(is_official: bool) -> SolutionResults:
     start_time = time.time()
     data = extract_data_from_file(2, is_official)
     rows = get_list_of_lines(data)
     part_1 = calculate_total_safe_reports(rows)
-    part_2 = len(rows)
+    part_2 = calculate_total_safe_reports_with_dampener(rows)
     end_time = time.time()
     execution_time = end_time - start_time
     results = SolutionResults(2, part_1, part_2, execution_time)
