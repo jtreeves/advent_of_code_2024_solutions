91168bac577ec5c16314029643fd6e1b9256e554
jtreeves <jackson.t.reeves@gmail.com>
2024-12-03 00:20:04 -0500

    Solve part 1 for day 3

--- day_3/solution.py
+++ day_3/solution.py
@@ -1,15 +1,37 @@
 import time
+from re import findall
 from utils.extract_data_from_file import extract_data_from_file
-from utils.get_list_of_lines import get_list_of_lines
 from utils.SolutionResults import SolutionResults
+
+
+def find_all_mul_expressions(corrupted_input: str) -> list[str]:
+    mul_pattern = r"mul\(\d+,\d+\)"
+    mul_expressions = findall(mul_pattern, corrupted_input)
+    return mul_expressions
+
+
+def multiply_values_in_expression(expression: str) -> int:
+    sections = expression.split(",")
+    first_number = int(sections[0][4:])
+    second_number = int(sections[1][:-1])
+    product = first_number * second_number
+    return product
+
+
+def sum_all_products(data: str) -> int:
+    total = 0
+    expressions = find_all_mul_expressions(data)
+    for expression in expressions:
+        product = multiply_values_in_expression(expression)
+        total += product
+    return total
 
 
 def solve_problem(is_official: bool) -> SolutionResults:
     start_time = time.time()
     data = extract_data_from_file(3, is_official)
-    rows = get_list_of_lines(data)
-    part_1 = len(rows)
-    part_2 = len(rows)
+    part_1 = sum_all_products(data)
+    part_2 = len(data)
     end_time = time.time()
     execution_time = end_time - start_time
     results = SolutionResults(3, part_1, part_2, execution_time)
